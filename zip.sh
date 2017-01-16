88bac1e7cb58436ea33760768586f46e49ef05aa
Radhika Karthik <Radhika.Karthik@teradata.com>
2017-01-16 15:07:21 -0800

    Adding Zip.sh to to upload the DM_Cluster_Marketplace.zip file to marketplace

---
+++ zip.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm -f ../DM_Cluster_Internal.zip
+zip ../DM_Cluster_Internal.zip *.json
+zip ../DM_Cluster_Internal.zip *.py
+zip ../DM_Cluster_Internal.zip *.sh
+zip ../DM_Cluster_Internal.zip README.md
